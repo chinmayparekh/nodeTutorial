4de1830760b24b2fda800d7f20000e6d2de45904
chinmayparekh <chinmayparekh11@gmail.com>
2022-06-16 18:49:48 +0530

    added routes for promo,leadership and dishId

--- node-express/index.js
+++ node-express/index.js
@@ -7,36 +7,41 @@ const hostname = "localhost";
 const port = 3000;
 const bodyParser = require("body-parser");
 const app = express();
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
+
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use('/dishes',dishRouter);//all requests coming to /dishes will be handled by dishRouter
-app.all("/dishes", (req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next();
-});
+// app.all("/dishes", (req, res, next) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   next();
+// });
 
-app.get("/dishes", (req, res, next) => {
-  res.end("Will send all the dishes to you!");
-});
+// app.get("/dishes", (req, res, next) => {
+//   res.end("Will send all the dishes to you!");
+// });
 
-app.post("/dishes", (req, res, next) => {
-  res.end(
-    "Will add the dish: " +
-      req.body.name +
-      " with details: " +
-      req.body.description
-  );
-});
+// app.post("/dishes", (req, res, next) => {
+//   res.end(
+//     "Will add the dish: " +
+//       req.body.name +
+//       " with details: " +
+//       req.body.description
+//   );
+// });
 
-app.put("/dishes", (req, res, next) => {
-  res.statusCode = 403;
-  res.end("PUT operation not supported on /dishes");
-});
+// app.put("/dishes", (req, res, next) => {
+//   res.statusCode = 403;
+//   res.end("PUT operation not supported on /dishes");
+// });
 
-app.delete("/dishes", (req, res, next) => {
-  res.end("Deleting all dishes");
-});
+// app.delete("/dishes", (req, res, next) => {
+//   res.end("Deleting all dishes");
+// });
 
 // app.get("/dishes/:dishId", (req, res, next) => {
 //   res.end("Will send details of the dish: " + req.params.dishId + " to you!");
---
+++ node-express/routes/leaderRouter.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+//01//////////////////////leaders
+
+leaderRouter.route('/')//mount this express router at the /dishes endpoint.chain all GET PUT POST DELET method already do this router
+
+.all((req,res,next) => {//‘/dishes' endpoint,/dishes REST API endpoint，(req,res，next）callback function)
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');//send plaintext replies back to client.
+    next();//looking for additional specifications down below here which will match /dishes endpoint
+})
+
+.get((req,res,next) => {
+    res.end('Will send all the leaders info to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+})//parse body and added into the req object as req.body
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leaders');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all leaders info');
+});
+
+//02//////////////////////leaderId
+
+leaderRouter.route('/:leaderId') 
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();//looking for additional specifications down below here which will match /dishes endpoint
+})
+
+.get((req,res,next) => {
+    res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+})
+
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /leaders/'+ req.params.leaderId);
+})
+
+.put((req, res, next) => {
+  res.write('Updating the leader: ' + req.params.leaderId + '\n');
+  res.end('Will update the leader: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+})
+
+.delete((req, res, next) => {
+    res.end('Deleting leader: ' + req.params.leaderId);
+});
+
+
+module.exports = leaderRouter ;
